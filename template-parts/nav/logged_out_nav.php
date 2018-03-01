cff9da96d24f1e9ccff0bafc8460bc4a805288b4
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-01 00:15:07 +0000

    logged out header goes to main site

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,7 +1,7 @@
 <div class="tgc_blue_topbar"></div>
 <div data-collapse="medium" data-animation="default" data-duration="1000" class="tgc_navbar_light w-nav">
 <div class="w-container">
-  <a href="/" class="w-nav-brand">
+  <a href="https://www.thegameclass.com" class="w-nav-brand">
     <h3 class="tgc_brand_light">The Game Class</h3>
   </a>
   <nav role="navigation" class="nav-menu w-nav-menu">
